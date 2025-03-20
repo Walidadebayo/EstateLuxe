e4c20e6be18cb9c2ad819857c4b044354698c1f1
Walid Adebayo <adebayowalid@gmail.com>
2025-03-20 13:36:32 +0000

    chore: update dependencies and add new Kendo React packages
    - Added "@progress/kendo-react-dialogs" and "@progress/kendo-react-layout" to package.json.
    - Updated Next.js version from 15.2.2 to ^14.2.25.
    - Downgraded React and React DOM from ^19.0.0 to ^18.3.1.
    - Updated TypeScript types for React and React DOM to match the new versions.
    - Modified tsconfig.json to include "next.config.mjs" instead of "next.config.ts"
    - Moved next.config.ts to next.config.mjs to support the new downgrade version

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
-import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
   images: {
     unoptimized: true,
@@ -14,7 +14,7 @@ const nextConfig: NextConfig = {
     //     hostname: "placehold.co",
     //   }
     // ],
-  }  
+  },
 };
 
 export default nextConfig;
